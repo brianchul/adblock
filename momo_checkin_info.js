355165e8ed8e26a3304e41cf023b94d68b020348
brianchul <18157275+brianchul@users.noreply.github.com>
2022-11-03 14:50:55 +0800

    Add files via upload

---
+++ momo_checkin_info.js
@@ -0,0 +1,41 @@
+function momoNotify(subtitle = '', message = '') {
+  $notify('🍑 Momo token', subtitle, message, { 'url': 'momo.app://' });
+};
+
+if ($request.method === 'POST') {
+  const cookie = $request.headers['Cookie'] || $request.headers['cookie'];
+  if (cookie && $request.body) {
+    try {
+      let body = JSON.parse($request.body);
+      if (body.doAction === 'list') {
+        body.pNo = '';
+        body.doAction = 'reg';
+        
+        const saveCookie = $prefs.setValueForKey(cookie, 'momoCookie');
+        const saveBody = $prefs.setValueForKey(JSON.stringify(body), 'momoBody');
+        if (!(saveCookie && saveBody)) {
+          momoNotify(
+            '保存失敗 ‼️',
+            '請稍後嘗試'
+          );
+        } else {
+          momoNotify(
+            '保存成功 🍪',
+            ''
+          );
+        }
+      }
+    } catch (error) {
+      momoNotify(
+        '保存失敗 ‼️',
+        error
+      );
+    }
+  } else {
+    momoNotify(
+      '保存失敗 ‼️',
+      '請重新登入'
+    );
+  }
+}
+$done({})
